9cad599f90d2c999c86c8cc10b26db9e045a2dc7
AbhinavDasari <anandamvenkatanagasai@gmail.com>
2025-04-09 07:40:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ if uploaded_file is not None:
 
     # Display Results
     st.success(f"✅ **Prediction: {predicted_class}**")
-   # st.write(f"🧪 **Confidence Score:** `{confidence:.4f}`")
+    st.write(f"🧪 **Confidence Score:** `{confidence:.4f}`")
 
     # Show extra message based on prediction
     if prediction[0][0] > 0.5:
